4be1fb79a0ad04b4cca339bbfb824d3e6ab4edb9
hamedfathi <noeffect@outlook.com>
2021-01-04 22:15:18 +0330

    feat: Cult.Configuration added

---
+++ Cult.Configuration/ConfigurationUtility.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Linq;
+
+namespace Cult.Configuration
+{
+    public static class ConfigurationUtility
+    {
+        public static IConfigurationRoot Build()
+        {
+            var args = Environment.GetCommandLineArgs();
+            var envArg = args.ToList().IndexOf("--environment");
+            var envFromArgs = envArg >= 0 ? args[envArg + 1] : null;
+
+            var aspnetcore = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+            var dotnetcore = Environment.GetEnvironmentVariable("DOTNET_ENVIRONMENT");
+
+            var environment = envFromArgs ?? (string.IsNullOrWhiteSpace(aspnetcore)
+                ? dotnetcore
+                : aspnetcore);
+
+            return new ConfigurationBuilder()
+                .AddCommandLine(Environment.GetCommandLineArgs())
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddJsonFile(
+                    $"appsettings.{environment}.json",
+                    optional: true)
+                .Build();
+        }
+    }
+}
