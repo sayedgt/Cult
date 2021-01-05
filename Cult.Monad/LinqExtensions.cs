0731b8374abf2cfd5f327538f5330f46edabb9c9
hamedfathi <noeffect@outlook.com>
2021-01-06 00:03:02 +0330

    feat: Cult.Monad added

---
+++ Cult.Monad/LinqExtensions.cs
@@ -0,0 +1,10 @@
+using Cult.Monad;
+// ReSharper disable All
+
+namespace System.Linq
+{
+    public static class LinqExtensions
+    {
+        public static Maybe<C> SelectMany<A, B, C>(this Maybe<A> ma, Func<A, Maybe<B>> f, Func<A, B, C> select) => ma.Bind(a => f(a).Map(b => select(a, b)));
+    }
+}
