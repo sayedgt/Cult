f0be5ad7b867d71fb6e9271add75ecce2bac1d60
hamedfathi <noeffect@outlook.com>
2021-01-07 14:43:08 +0330

    feat: Cult.TypeScript added - BigMath & Figlet removed

---
+++ Cult.TypeScript/TypeScriptExtensions.cs
@@ -0,0 +1,34 @@
+﻿using CsToTs;
+using System;
+using System.Collections.Generic;
+
+namespace Cult.TypeScript
+{
+    public static class TypeScriptExtensions
+    {
+        public static string ToTypeScript(this Type type)
+        {
+            return Generator.GenerateTypeScript(type, new TypeScriptOptions());
+        }
+        public static string ToTypeScript(this Type type, TypeScriptOptions options)
+        {
+            return Generator.GenerateTypeScript(type, options);
+        }
+        public static string ToTypeScript(this IEnumerable<Type> types)
+        {
+            return Generator.GenerateTypeScript(types);
+        }
+        public static string ToTypeScript(this IEnumerable<Type> types, TypeScriptOptions options)
+        {
+            return Generator.GenerateTypeScript(options, types);
+        }
+        public static string ToTypeScript(this Type[] types, TypeScriptOptions options)
+        {
+            return Generator.GenerateTypeScript(options, types);
+        }
+        public static string ToTypeScript(this Type[] types)
+        {
+            return Generator.GenerateTypeScript(types);
+        }
+    }
+}
