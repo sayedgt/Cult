6ef0765ea3b6fa63782e3d6ce6010db774e562d4
hamedfathi <noeffect@outlook.com>
2021-01-16 21:50:42 +0330

    feat: add extension methods for exceptions.

---
+++ Cult.Extensions/ExceptionExtensions.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Cult.Extensions.ExtraException
+{
+    public static class ExceptionExtensions
+    {
+        public static string GetFullMessage(this Exception exception)
+        {
+            return exception.InnerException == null
+                 ? exception.Message
+                 : exception.Message + " --> " + exception.InnerException.GetFullMessage();
+        }
+
+        public static string ToFormattedString(this Exception exception)
+        {
+            IEnumerable<string> messages = exception
+                .GetAllExceptions()
+                .Where(e => !string.IsNullOrWhiteSpace(e.Message))
+                .Select(e => e.Message.Trim());
+            return string.Join(Environment.NewLine, messages);
+        }
+
+        private static IEnumerable<Exception> GetAllExceptions(this Exception exception)
+        {
+            yield return exception;
+
+            if (exception is AggregateException aggrEx)
+            {
+                foreach (Exception innerEx in aggrEx.InnerExceptions.SelectMany(e => e.GetAllExceptions()))
+                {
+                    yield return innerEx;
+                }
+            }
+            else if (exception.InnerException != null)
+            {
+                foreach (Exception innerEx in exception.InnerException.GetAllExceptions())
+                {
+                    yield return innerEx;
+                }
+            }
+        }
+    }
+}
