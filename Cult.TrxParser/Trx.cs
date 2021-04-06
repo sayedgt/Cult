172d188b6f1048ac1e8aa632891af1a1dcea29f8
hamedfathi <noeffect@outlook.com>
2021-04-06 23:32:58 +0430

    chore: Trx to markdown

--- Cult.TrxParser/Trx.cs
+++ Cult.TrxParser/Trx.cs
@@ -1,4 +1,6 @@
 ﻿using System.IO;
+using System.Linq;
+using System.Text;
 using System.Text.RegularExpressions;
 using System.Xml.Linq;
 using System.Xml.Serialization;
@@ -27,6 +29,30 @@ namespace Cult.TrxParser
                 entity = (TestRun)xs.Deserialize(sr);
             }
             return entity;
+        }
+
+        public static string ToMarkdown(TestRun testRun)
+        {
+            var sb = new StringBuilder();
+            var groups = testRun.Results.UnitTestResults
+                .GroupBy(x => x.TestId)
+                .ToList();
+            foreach (var group in groups)
+            {
+                var testName = @group.FirstOrDefault()?.TestName;
+                var name = testName?.Substring(0, testName.IndexOf('('));
+                sb.AppendLine($"## {name}");
+                var i = 0;
+                foreach (var g in @group.OrderBy(x => x.StartTime))
+                {
+                    if (testName == null) continue;
+                    var text = g.TestName.Substring(testName.IndexOf(')') + 1).Trim();
+                    sb.AppendLine($"{++i}. {text}");
+                }
+                sb.AppendLine();
+            }
+
+            return sb.ToString();
         }
 
         /// <summary>
