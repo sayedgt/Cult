f18ad6701491fcc2527500e4ab5dda29dea245b7
hamedfathi <noeffect@outlook.com>
2021-04-16 01:35:48 +0430

    chore: add IMemoryCache extensions

--- Cult.MemoryCache/MemoryCacheExtensions.cs
+++ Cult.MemoryCache/MemoryCacheExtensions.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Runtime.Caching;
+using Microsoft.Extensions.Caching.Memory;
+
 // ReSharper disable All 
 namespace Cult.MemoryCache
 {
@@ -37,6 +39,59 @@ namespace Cult.MemoryCache
                 return memoryCache[key];
 
             return memoryCache[key] = objectToCache;
+        }
+
+        public static TReturn SafeGet<TReturn>(this IMemoryCache memoryCache, string key, Func<TReturn> objectToCache)
+        {
+            if (memoryCache.TryGetValue(key, out TReturn cached))
+            {
+                return cached;
+            }
+            else
+            {
+                TReturn data = objectToCache();
+                memoryCache.Set(key, data);
+                return data;
+            }
+        }
+        public static TReturn SafeGet<TReturn>(this IMemoryCache memoryCache, string key, TReturn objectToCache)
+        {
+            if (memoryCache.TryGetValue(key, out TReturn cached))
+            {
+                return cached;
+            }
+            else
+            {
+                memoryCache.Set(key, objectToCache);
+                return objectToCache;
+            }
+        }
+        public static object SafeGet(this IMemoryCache memoryCache, string key, Func<object> objectToCache)
+        {
+
+            if (memoryCache.TryGetValue(key, out var cached))
+            {
+                return cached;
+            }
+            else
+            {
+                var data = objectToCache();
+                memoryCache.Set(key, data);
+                return data;
+            }
+
+        }
+        public static object SafeGet(this IMemoryCache memoryCache, string key, object objectToCache)
+        {
+            if (memoryCache.TryGetValue(key, out var cached))
+            {
+                return cached;
+            }
+            else
+            {
+                memoryCache.Set(key, objectToCache);
+                return objectToCache;
+            }
         }
     }
 }
