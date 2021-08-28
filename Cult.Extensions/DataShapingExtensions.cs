8d59e25a1828daef874b861a1ac652170bc0872a
hamedfathi <hamedfathi@outlook.com>
2021-08-28 10:38:28 +0430

    chore: DataShaping with ignoreCase option

--- Cult.Extensions/DataShapingExtensions.cs
+++ Cult.Extensions/DataShapingExtensions.cs
@@ -7,7 +7,7 @@ namespace Cult.Extensions.DataShaping
 {
     public static class DataShapingExtensions
     {
-        public static IDictionary<string, object> ShapeData<T>(this T dataToshape, string fields)
+        public static IDictionary<string, object> ShapeData<T>(this T dataToshape, string fields, bool ignoreCase = true)
         {
             var result = new Dictionary<string, object>();
             if (dataToshape == null)
@@ -15,14 +15,14 @@ namespace Cult.Extensions.DataShaping
                 return result;
             }
 
-            var propertyInfoList = GetPropertyInfos<T>(fields);
+            var propertyInfoList = GetPropertyInfos<T>(fields, ignoreCase);
 
             result.FillDictionary(dataToshape, propertyInfoList);
 
             return result;
         }
 
-        public static IDictionary<string, object> ShapeData<T>(this IEnumerable<T> dataToshape, string fields, string jsonListName = "values")
+        public static IDictionary<string, object> ShapeData<T>(this IEnumerable<T> dataToshape, string fields, bool ignoreCase = true, string jsonListName = "values")
         {
             var result = new Dictionary<string, object>();
             if (dataToshape == null)
@@ -30,7 +30,7 @@ namespace Cult.Extensions.DataShaping
                 return result;
             }
 
-            var propertyInfoList = GetPropertyInfos<T>(fields);
+            var propertyInfoList = GetPropertyInfos<T>(fields, ignoreCase);
 
             var list = dataToshape.Select(line =>
             {
@@ -44,14 +44,14 @@ namespace Cult.Extensions.DataShaping
             return result;
         }
 
-        public static string ToShapedData<T>(this T dataToshape, string fields)
+        public static string ToShapedData<T>(this T dataToshape, string fields, bool ignoreCase = true)
         {
-            return JsonSerializer.Serialize(ShapeData(dataToshape, fields));
+            return JsonSerializer.Serialize(ShapeData(dataToshape, fields, ignoreCase));
         }
 
-        public static string ToShapedData<T>(this IEnumerable<T> dataToshape, string fields, string jsonListName = "values")
+        public static string ToShapedData<T>(this IEnumerable<T> dataToshape, string fields, string jsonListName = "values", bool ignoreCase = true)
         {
-            return JsonSerializer.Serialize(ShapeData(dataToshape, fields, jsonListName));
+            return JsonSerializer.Serialize(ShapeData(dataToshape, fields, ignoreCase, jsonListName));
         }
 
         private static void FillDictionary<T>(this IDictionary<string, object> dictionary, T source, IEnumerable<PropertyInfo> fields)
@@ -64,13 +64,13 @@ namespace Cult.Extensions.DataShaping
             }
         }
 
-        private static IEnumerable<PropertyInfo> GetPropertyInfos<T>(string fields)
+        private static IEnumerable<PropertyInfo> GetPropertyInfos<T>(string fields, bool ignoreCase)
         {
             var propertyInfoList = new List<PropertyInfo>();
 
             if (!string.IsNullOrWhiteSpace(fields))
             {
-                return ExtractSelectedPropertiesInfo<T>(fields, propertyInfoList);
+                return ExtractSelectedPropertiesInfo<T>(fields, propertyInfoList, ignoreCase);
             }
 
             var propertyInfos = typeof(T).GetRuntimeProperties();
@@ -78,13 +78,14 @@ namespace Cult.Extensions.DataShaping
             return propertyInfoList;
         }
 
-        private static IEnumerable<PropertyInfo> ExtractSelectedPropertiesInfo<T>(string fields, List<PropertyInfo> propertyInfoList)
+        private static IEnumerable<PropertyInfo> ExtractSelectedPropertiesInfo<T>(string fields, List<PropertyInfo> propertyInfoList, bool ignoreCase)
         {
             var fieldsAfterSplit = fields.Split(',');
 
             foreach (var propertyName in fieldsAfterSplit.Select(f => f.Trim()))
             {
-                var propertyInfo = typeof(T).GetRuntimeProperty(propertyName);
+                var propName = ignoreCase ? propertyName.ToLower() : propertyName;
+                var propertyInfo = typeof(T).GetRuntimeProperties().FirstOrDefault(x => (ignoreCase ? x.Name.ToLower() : x.Name) == propName);
 
                 if (propertyInfo == null)
                 {
