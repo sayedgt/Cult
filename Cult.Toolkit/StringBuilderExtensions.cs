b6bcdee95ee8f335e0e19fc64c405d77045a5dfb
hamedfathi <hamedfathi@outlook.com>
2021-10-13 02:05:17 +0330

    chore: more extensions

--- Cult.Toolkit/StringBuilderExtensions.cs
+++ Cult.Toolkit/StringBuilderExtensions.cs
@@ -1,4 +1,3 @@
-using static System.String;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -7,6 +6,128 @@ namespace Cult.Toolkit.ExtraStringBuilder
 {
     public static class StringBuilderExtensions
     {
+        public static StringBuilder AppendFormatIf(this StringBuilder sb, bool condition, string format,
+                                  params object[] args)
+        {
+            if (condition) sb.AppendFormat(format, args);
+            return sb;
+        }
+        public static void AppendIf(this StringBuilder sb, bool condition, char text)
+        {
+            if (condition)
+            {
+                sb.Append(text);
+            }
+        }
+        public static void AppendIf(this StringBuilder sb, bool condition, string text)
+        {
+            if (condition)
+            {
+                sb.Append(text);
+            }
+        }
+        public static StringBuilder AppendIf<T>(this StringBuilder @this, Func<T, bool> predicate, params T[] values)
+        {
+            foreach (var value in values)
+            {
+                if (predicate(value))
+                {
+                    @this.Append(value);
+                }
+            }
+            return @this;
+        }
+        public static StringBuilder AppendIf(this StringBuilder sb, bool condition, object value)
+        {
+            if (condition) sb.Append(value);
+            return sb;
+        }
+        public static StringBuilder AppendJoin<T>(this StringBuilder @this, string separator, IEnumerable<T> values)
+        {
+            @this.Append(string.Join(separator, values));
+            return @this;
+        }
+        public static StringBuilder AppendJoin<T>(this StringBuilder @this, string separator, params T[] values)
+        {
+            @this.Append(string.Join(separator, values));
+            return @this;
+        }
+        public static void AppendLine(this StringBuilder builder, string value, params object[] parameters)
+        {
+            builder.AppendLine(string.Format(value, parameters));
+        }
+        public static StringBuilder AppendLineFormat(this StringBuilder @this, string format, params object[] args)
+        {
+            @this.AppendLine(string.Format(format, args));
+            return @this;
+        }
+        public static StringBuilder AppendLineFormat(this StringBuilder @this, string format, List<IEnumerable<object>> args)
+        {
+            @this.AppendLine(string.Format(format, args));
+            return @this;
+        }
+        public static StringBuilder AppendLineIf(this StringBuilder sb, bool condition, string value)
+        {
+            if (condition) sb.AppendLine(value);
+            return sb;
+        }
+        public static StringBuilder AppendLineIf<T>(this StringBuilder @this, Func<T, bool> predicate, params T[] values)
+        {
+            foreach (var value in values)
+            {
+                if (predicate(value))
+                {
+                    @this.AppendLine(value.ToString());
+                }
+            }
+            return @this;
+        }
+        public static StringBuilder AppendLineIf(this StringBuilder sb, bool condition, object value)
+        {
+            if (condition) sb.AppendLine(value.ToString());
+            return sb;
+        }
+        public static StringBuilder AppendLineIf(this StringBuilder sb, bool condition, string format, params object[] args)
+        {
+            if (condition) sb.AppendFormat(format, args).AppendLine();
+            return sb;
+        }
+        public static StringBuilder AppendLineJoin<T>(this StringBuilder @this, string separator, IEnumerable<T> values)
+        {
+            @this.AppendLine(string.Join(separator, values));
+            return @this;
+        }
+        public static StringBuilder AppendLineJoin(this StringBuilder @this, string separator, params object[] values)
+        {
+            @this.AppendLine(string.Join(separator, values));
+            return @this;
+        }
+        public static string Strip(this StringBuilder sb)
+        {
+            for (int i = sb.Length - 1; i >= 0 && sb[i] == ' '; --i)
+            {
+                sb.Remove(sb.Length - 1, 1);
+            }
+            sb.Remove(sb.Length - 1, 1);
+            return sb.ToString();
+        }
+        public static string Strip(this StringBuilder sb, int length)
+        {
+            if (length > sb.Length || length < 1)
+            {
+                return sb.ToString();
+            }
+            sb.Remove(sb.Length - length, length);
+            return sb.ToString();
+        }
+        public static string Substring(this StringBuilder @this, int startIndex)
+        {
+            return @this.ToString(startIndex, @this.Length - startIndex);
+        }
+        public static string Substring(this StringBuilder @this, int startIndex, int length)
+        {
+            return @this.ToString(startIndex, length);
+        }
         public static StringBuilder Reverse(this StringBuilder sb)
         {
             var start = 0;
@@ -23,91 +144,19 @@ namespace Cult.Toolkit.ExtraStringBuilder
         }
         public static void Append(this StringBuilder builder, string value, params object[] parameters)
         {
-            builder.Append(Format(value, parameters));
+            builder.Append(string.Format(value, parameters));
         }
         public static StringBuilder AppendFormat(this StringBuilder @this, string format, params object[] args)
         {
-            @this.Append(Format(format, args));
+            @this.Append(string.Format(format, args));
 
             return @this;
         }
         public static StringBuilder AppendFormat(this StringBuilder @this, string format, List<IEnumerable<object>> args)
         {
-            @this.Append(Format(format, args));
+            @this.Append(string.Format(format, args));
 
             return @this;
-        }
-        public static StringBuilder AppendIf<T>(this StringBuilder @this, Func<T, bool> predicate, params T[] values)
-        {
-            foreach (var value in values)
-            {
-                if (predicate(value))
-                {
-                    @this.Append(value);
-                }
-            }
-
-            return @this;
-        }
-        public static StringBuilder AppendJoin<T>(this StringBuilder @this, string separator, IEnumerable<T> values)
-        {
-            @this.Append(Join(separator, values));
-
-            return @this;
-        }
-        public static StringBuilder AppendJoin<T>(this StringBuilder @this, string separator, params T[] values)
-        {
-            @this.Append(Join(separator, values));
-
-            return @this;
-        }
-        public static void AppendLine(this StringBuilder builder, string value, params object[] parameters)
-        {
-            builder.AppendLine(Format(value, parameters));
-        }
-        public static StringBuilder AppendLineFormat(this StringBuilder @this, string format, params object[] args)
-        {
-            @this.AppendLine(Format(format, args));
-
-            return @this;
-        }
-        public static StringBuilder AppendLineFormat(this StringBuilder @this, string format, List<IEnumerable<object>> args)
-        {
-            @this.AppendLine(Format(format, args));
-
-            return @this;
-        }
-        public static StringBuilder AppendLineIf<T>(this StringBuilder @this, Func<T, bool> predicate, params T[] values)
-        {
-            foreach (var value in values)
-            {
-                if (predicate(value))
-                {
-                    @this.AppendLine(value.ToString());
-                }
-            }
-
-            return @this;
-        }
-        public static StringBuilder AppendLineJoin<T>(this StringBuilder @this, string separator, IEnumerable<T> values)
-        {
-            @this.AppendLine(Join(separator, values));
-
-            return @this;
-        }
-        public static StringBuilder AppendLineJoin(this StringBuilder @this, string separator, params object[] values)
-        {
-            @this.AppendLine(Join(separator, values));
-
-            return @this;
-        }
-        public static string Substring(this StringBuilder @this, int startIndex)
-        {
-            return @this.ToString(startIndex, @this.Length - startIndex);
-        }
-        public static string Substring(this StringBuilder @this, int startIndex, int length)
-        {
-            return @this.ToString(startIndex, length);
-        }
+        }      
     }
 }
--- Cult.Toolkit/TaskExtensions.cs
+++ Cult.Toolkit/TaskExtensions.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Threading;
 using System.Threading.Tasks;
 // ReSharper disable All 
 namespace Cult.Toolkit.ExtraTask
@@ -15,6 +17,95 @@ namespace Cult.Toolkit.ExtraTask
             {
                 onException(e);
             }
+        }
+
+        public static Task<V> GroupJoin<T, U, K, V>(
+                            this Task<T> source, Task<U> inner,
+                            Func<T, K> outerKeySelector, Func<U, K> innerKeySelector,
+                            Func<T, Task<U>, V> resultSelector)
+        {
+            return source.TaskBind(t =>
+            {
+                return resultSelector(
+                    t,
+                    inner.Where(u =>
+                        EqualityComparer<K>.Default.Equals(
+                            outerKeySelector(t),
+                            innerKeySelector(u)
+                            )
+                        )
+                    ).TaskUnit();
+            }
+                );
+        }
+
+        public static Task<V> Join<T, U, K, V>(
+                            this Task<T> source, Task<U> inner,
+                            Func<T, K> outerKeySelector, Func<U, K> innerKeySelector,
+                            Func<T, U, V> resultSelector)
+        {
+            Task.WaitAll(source, inner);
+
+            return source.TaskBind(t =>
+            {
+                return inner.TaskBind(u =>
+                {
+                    if (!EqualityComparer<K>.Default.Equals(outerKeySelector(t), innerKeySelector(u)))
+                        throw new OperationCanceledException();
+
+                    return resultSelector(t, u).TaskUnit();
+                }
+                    );
+            }
+                );
+        }
+
+        public static Task<U> Select<T, U>(
+                            this Task<T> source, Func<T, U> selector)
+        {
+            return source.TaskBind(t => selector(t).TaskUnit());
+        }
+
+        public static Task<C> SelectMany<A, B, C>(
+                            this Task<A> monad,
+                            Func<A, Task<B>> function,
+                            Func<A, B, C> projection)
+        {
+
+            return monad.TaskBind(
+                outer => function(outer).TaskBind(
+                    inner => projection(outer, inner).TaskUnit()));
+        }
+
+        public static Task<V> TaskBind<U, V>(
+                            this Task<U> m, Func<U, Task<V>> k)
+        {
+            return m.ContinueWith(m_ => k(m_.Result)).Unwrap();
+        }
+
+        public static Task<T> TaskUnit<T>(this T value)
+        {
+            return Task.Factory.StartNew(() => value);
+        }
+
+        public static bool WaitCancellationRequested(
+                            this CancellationToken token,
+                            TimeSpan timeout)
+        {
+            return token.WaitHandle.WaitOne(timeout);
+        }
+
+        public static Task<T> Where<T>(
+                            this Task<T> source, Func<T, bool> predicate)
+        {
+            return source.TaskBind(t =>
+            {
+                if (!predicate(t))
+                    throw new OperationCanceledException();
+
+                return t.TaskUnit();
+            }
+                );
         }
     }
 }
