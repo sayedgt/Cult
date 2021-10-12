ce1f5b0df8f3f9c9b5c86c4a524a5af0d094e0d5
hamedfathi <hamedfathi@outlook.com>
2021-10-12 21:32:33 +0330

    feat: Cult.Cryptography removed

---
+++ Cult.Toolkit/HashExtensions.cs
@@ -0,0 +1,118 @@
+﻿using Cult.Toolkit.ExtraStream;
+using Cult.Toolkit.ExtraString;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Cult.Toolkit.Cryptography
+{
+    public static class HashExtensions
+    {
+        public static string ComputeMD5Hash(this string data)
+        {
+            return ComputeMD5Hash(data.ToByteArray());
+        }
+        public static string ComputeMD5Hash(this Stream data)
+        {
+            return ComputeMD5Hash(data.ToByteArray());
+        }
+        public static string ComputeMD5Hash(this byte[] data)
+        {
+            using (MD5 md5Hash = MD5.Create())
+            {
+                byte[] bytes = md5Hash.ComputeHash(data);
+                StringBuilder builder = new StringBuilder();
+                for (int i = 0; i < bytes.Length; i++)
+                {
+                    builder.Append(bytes[i].ToString("x2"));
+                }
+                return builder.ToString();
+            }
+        }
+        public static string ComputeSHA1Hash(this string data)
+        {
+            return ComputeSHA1Hash(data.ToByteArray());
+        }
+        public static string ComputeSHA1Hash(this Stream data)
+        {
+            return ComputeSHA1Hash(data.ToByteArray());
+        }
+        public static string ComputeSHA1Hash(this byte[] data)
+        {
+            using (SHA1 sha1Hash = SHA1.Create())
+            {
+                byte[] bytes = sha1Hash.ComputeHash(data);
+                StringBuilder builder = new StringBuilder();
+                for (int i = 0; i < bytes.Length; i++)
+                {
+                    builder.Append(bytes[i].ToString("x2"));
+                }
+                return builder.ToString();
+            }
+        }
+        public static string ComputeSHA256Hash(this string data)
+        {
+            return ComputeSHA256Hash(data.ToByteArray());
+        }
+        public static string ComputeSHA256Hash(this Stream data)
+        {
+            return ComputeSHA256Hash(data.ToByteArray());
+        }
+        public static string ComputeSHA256Hash(this byte[] data)
+        {
+            using (SHA256 sha256Hash = SHA256.Create())
+            {
+                byte[] bytes = sha256Hash.ComputeHash(data);
+                StringBuilder builder = new StringBuilder();
+                for (int i = 0; i < bytes.Length; i++)
+                {
+                    builder.Append(bytes[i].ToString("x2"));
+                }
+                return builder.ToString();
+            }
+        }
+        public static string ComputeSHA384Hash(this string data)
+        {
+            return ComputeMD5Hash(data.ToByteArray());
+        }
+        public static string ComputeSHA384Hash(this Stream data)
+        {
+            return ComputeMD5Hash(data.ToByteArray());
+        }
+        public static string ComputeSHA384Hash(this byte[] data)
+        {
+            using (SHA384 sha384Hash = SHA384.Create())
+            {
+                byte[] bytes = sha384Hash.ComputeHash(data);
+                StringBuilder builder = new StringBuilder();
+                for (int i = 0; i < bytes.Length; i++)
+                {
+                    builder.Append(bytes[i].ToString("x2"));
+                }
+                return builder.ToString();
+            }
+        }
+        public static string ComputeSHA512Hash(this string data)
+        {
+            return ComputeMD5Hash(data.ToByteArray());
+        }
+        public static string ComputeSHA512Hash(this Stream data)
+        {
+            return ComputeMD5Hash(data.ToByteArray());
+        }
+        public static string ComputeSHA512Hash(this byte[] data)
+        {
+            using (SHA512 sha512Hash = SHA512.Create())
+            {
+                byte[] bytes = sha512Hash.ComputeHash(data);
+                StringBuilder builder = new StringBuilder();
+                for (int i = 0; i < bytes.Length; i++)
+                {
+                    builder.Append(bytes[i].ToString("x2"));
+                }
+                return builder.ToString();
+            }
+        }
+
+    }
+}
